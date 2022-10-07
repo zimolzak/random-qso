b249c29852c44a94530fdd08bf2e883de70b1443
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-10-07 09:26:28 -0500

    Random call signs and weather.

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from random import choice
 from random import randint
+from string import ascii_uppercase
 
 MY_CALL = 'KI5XYZ'
 
 
+def random_suffix():
+    uppercase_plus_null = list(ascii_uppercase) + ['']
+    return choice(ascii_uppercase) + choice(ascii_uppercase) + choice(uppercase_plus_null)
+
+
 class RandomOperator:
     def __init__(self):
-        self.call = 'KG4ABC'
+        self.call = '%s%s%s' % (
+            choice('KG KI W K KA'.split()),
+            randint(0, 9),
+            random_suffix()
+        )
         city_state = choice([['Phoenix', 'AZ'],
                              ['Troy', 'MI'],
                              ['St Paul', 'MN'],
@@ -18,7 +28,11 @@ class RandomOperator:
         self.name = choice('John Phil Bob Mary Ali Alex Tom Dennis Lou Leo Larry Ann'.split())
         self.rig = choice(['IC 7300', 'Kenwood TS 940S', 'TS 890S', 'Yaesu FT 1000',
                            'FT DX 101D', 'FT DX 10', 'Yaesu FT 101'])
-        self.wx = 'rainy 65 deg'
+        self.wx = '%s%s %i deg' % (
+            choice('rainy cloudy clr'.split()),
+            choice([' es', '']),
+            randint(35, 99)
+        )
         self.skcc = 'skcc %s%s%i%s' % (
             choice(['nr ', '']),
             choice(['is ', '']),
