abb939a020c5676b0901e3f9f275208d4b74f73b
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-10-07 09:12:49 -0500

    Random qth.

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ MY_CALL = 'KI5XYZ'
 class RandomOperator:
     def __init__(self):
         self.call = 'KG4ABC'
-        self.qth = 'Phoenix Phoenix AZ AZ'
+        city_state = choice([['Phoenix', 'AZ'],
+                             ['Troy', 'MI'],
+                             ['St Paul', 'MN'],
+                             ['San Diego', 'CA'],
+                             ['Asheville', 'NC'],
+                             ['Austin', 'TX'],
+                             ['Omaha', 'NE']])
+        self.qth = '%s %s %s %s' % (city_state[0], city_state[0], city_state[1], city_state[1])
         self.name = choice('John Phil Bob Mary Ali Alex Tom Dennis Lou Leo Larry Ann'.split())
         self.rig = choice(['IC 7300', 'Kenwood TS 940S', 'TS 890S', 'Yaesu FT 1000',
                            'FT DX 101D', 'FT DX 10', 'Yaesu FT 101'])
