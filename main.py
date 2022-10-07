b2b3d9ab24dbcaed5f0b75691f5d2e1ba9c016d5
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-10-07 08:52:38 -0500

    3 more fake details. Support for __str__().

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import choice
+from random import randint
 
 MY_CALL = 'KI5XYZ'
 
@@ -6,8 +7,20 @@ MY_CALL = 'KI5XYZ'
 class RandomOperator:
     def __init__(self):
         self.call = 'KG4ABC'
-        self.qth = 'Phoenix'
+        self.qth = 'Phoenix Phoenix AZ AZ'
         self.name = 'Phil'
+        self.rig = 'IC 7300'
+        self.wx = 'rainy 65 deg'
+        self.skcc = 'skcc %s%s%i%s' % (
+            choice(['nr ', '']),
+            choice(['is ', '']),
+            randint(1000, 25000),
+            choice(['C', 'T', ''])
+        )
+
+    def __str__(self):
+        all_features = [self.call, self.qth, self.name, self.rig, self.wx, self.skcc]
+        return str(all_features)
 
 
 def intro(operator: RandomOperator):
@@ -18,3 +31,4 @@ def intro(operator: RandomOperator):
 if __name__ == '__main__':
     op = RandomOperator()
     print(intro(op))
+    print(op)
